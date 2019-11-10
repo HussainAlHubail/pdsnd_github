3117e476d934595ce5a6694848eb0303c9ceb47b
Hussain-AlHubail <hussainswe@gmail.com>
2019-11-11 01:09:11 +0300

    modified cities message comment

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    #### Using PyInquirer to have a fancy user interaction instead of bothering with typing :)
+    #### Using PyInquirer to have a fancy user interaction instead of bothering with typing
     #Getting City data
     questions = [
         {
